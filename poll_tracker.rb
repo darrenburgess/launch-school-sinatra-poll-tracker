9e9c9cfcf129141e9419f6d00b5cc0a82396dcad
Darren Burgess <darrentburgess@gmail.com>
2016-08-17 11:08:24 -0400

    initial commit

---
+++ poll_tracker.rb
@@ -0,0 +1,13 @@
+=begin
+requirements:
+- user can sign in/out
+- entering of polls
+  - date, leader, margin, firm
+- editing of polls
+- delete polls
+- show polling average
+  - by week
+  - by two weeks
+  - by month
+  - all polling
+=end
