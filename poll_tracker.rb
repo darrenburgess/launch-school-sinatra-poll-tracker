ba4a7f9659841f1b0ab59f212d0e1875ab018080
Darren Burgess <darrentburgess@gmail.com>
2016-08-19 09:01:03 -0400

    add database and basic confirmation tests for such

--- poll_tracker.rb
+++ poll_tracker.rb
@@ -3,13 +3,36 @@ require "sinatra/contrib"
 require "sinatra/reloader"
 require "yaml"
 require "bcrypt"
+require "data_mapper"
 require "pry"
+
+DataMapper.setup(:default, "sqlite3::memory")
 
 configure do
   enable :sessions
   set :session_secret, "super secret"
 end
 
+class Poll
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :clinton, Float
+  property :trump, Float
+end
+
+def test_database
+  Poll.auto_migrate!
+
+  first_poll = Poll.new
+  first_poll.clinton = 54.3
+  first_poll.trump = 48.1
+  first_poll.save
+end
+
 get "/" do
+  clinton = Poll.get(1).clinton
+  trump = Poll.get(1).trump
+  @poll = "Clinton: #{clinton}  Trump: #{trump}"
   erb :index
 end
